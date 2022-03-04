38e88ce33191ab356a6674800bf98ed0d544e063
marouane youssfi <marouaneyoussfi5@gmail.com>
2022-03-04 17:41:09 +0100

    add publish_timestamp field to handle if you want to publish the video add the current time

--- videos/admin.py
+++ videos/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import VideoAllProxy, VideoPublishedProxy
 
-# Register your models here.
+class VideoAllAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'state', 'video_id', 'is_published']
+    search_fields = ['title']
+    list_filter = ['state', 'active']
+    readonly_fields = ['id', 'is_published']
+    class Meta:
+        model = VideoAllProxy
+
+    """def published(self, obj, *args, **kwargs):
+        return obj.active"""
+admin.site.register(VideoAllProxy, VideoAllAdmin)
+
+
+class VideoPublishedProxyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'video_id']
+    search_fields = ['title']
+    # list_filter = ['video_id']
+    class Meta:
+        model = VideoPublishedProxy
+
+    def get_queryset(self, request):
+        return VideoPublishedProxy.objects.filter(active=True)
+
+
+admin.site.register(VideoPublishedProxy, VideoPublishedProxyAdmin)
