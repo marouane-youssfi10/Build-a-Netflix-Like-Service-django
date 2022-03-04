d6f4ab0c34d5efcf0dba2b24a00b4196118cdb01
marouane youssfi <marouaneyoussfi5@gmail.com>
2022-03-04 16:13:48 +0100

    add class to test.py

--- videos/tests.py
+++ videos/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Video
 
-# Create your tests here.
+
+class VideoModelTestCase(TestCase):
+    def setUp(self):
+        Video.objects.create(title='This is my title')
+
+    def test_valid_title(self):
+        title = 'This is my title'
+        qs = Video.objects.filter(title=title)
+        self.assertTrue(qs.exists())
